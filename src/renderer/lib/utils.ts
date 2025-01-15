c3b1aba645d13e7367bfe1a7e6c018659399b3ae
Gus <jianbaiye@outlook.com>
2025-01-16 01:20:07 +0800

    fix: remove api key valid

--- src/renderer/lib/utils.ts
+++ src/renderer/lib/utils.ts
@@ -6,12 +6,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function validateOpenAIKey(key: string) {
-  if (!key.startsWith('sk-')) {
-    return false;
-  }
+  // if (!key.startsWith('sk-')) {
+  //   return false;
+  // }
   // maybe wrong
-  if (key.length !== 51) {
-    return false;
-  }
+  // if (key.length !== 51) {
+  //   return false;
+  // }
   return true;
 }
